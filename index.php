fb21be395ab05b50e3ed07e8604a44041c5be0cf
PatrickMalara <pat_malara@outlook.com>
2021-11-21 16:23:37 -0500

    Minor visual updates. This will be an iterative design process

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+
+include "secrets.php"
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,114 +12,83 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sauer Stats</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&family=Press+Start+2P&display=swap" rel="stylesheet"> 
+
     <!-- <script src="main.js"></script> -->
-    <style>
-        * {
-            /* border: 1px solid gainsboro; */
-        }
-
-        #player-select {
-            padding: 20px;
-            display: block;
-            margin: auto;
-            margin-top: 50px;
-            width: 500px;
-        }
-
-        .player-container {
-            padding: 10px;
-            width: 100px;
-            font-size: 18px;
-            font-weight: 600;
-            text-transform: uppercase;
-            text-align: center;
-            border: 1px solid gainsboro;
-            transition: all 0.2s;
-
-        }
-
-        .player-container:hover {
-            background: #c1ffd7;
-            color: black;
-            transform: scaleX(1.05) scaleY(1.05);
-            box-shadow: 0px 8px 25px -1px #00000040;
-            cursor: pointer;
-        }
-
-        #submit-message {
-            color: red;
-        }
-
-        #stats-grid {
-            display: grid;
-            grid-auto-flow: column;
-            width: 500px;
-            margin: auto;
-            text-transform: capitalize;
-        }
-
-        #main {
-            margin: auto;
-        }
-
-        .lose {
-            color: red;
-        }
-
-        .win {
-            color: green;
-        }
-
-        .tie {
-            color: gray;
-        }
-    </style>
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/utilities.css">
 </head>
 
 <body>
+
+    <header class="flex align-center">
+        <div>
+        <img src="assets/cube2logo.png" style="width: 280px; margin-right: 2rem"/> 
+        </div>
+        <div>
+            <br/>
+            <input type="file" name="inputfile" id="inputfile">
+            <input type="button" id="submitMatch" value="Submit Match">
+            <p id='submit-message'></p>
+        </div>
+    </header>
+
     <div id="main">
-        <h1>Cube 2: Sauerbraten - Player Stats</h1>
-
-        <input type="file" name="inputfile" id="inputfile">
-        <input type="button" id="submitMatch" value="Submit Match">
-        <p id='submit-message'></p>
-
         <div id='player-select'>
-            <h2>Players</h2>
+            <h3>Players</h3>
+            <div class="flex">
+                <?php
+                try {
+                    $dbh = new PDO("mysql:host=localhost;dbname=".$dbname, $username, $password);
+                } catch (Exception $e) {
+                    die("ERROR. Couldn't get DB Connection. " . $e->getMessage());
+                }
+                $cmd = "select player_name from sauer_matches group by player_name";
+                $stmt = $dbh->prepare($cmd);
+                $success = $stmt->execute([]);
 
-            <?php
-            try {
-                $dbh = new PDO("mysql:host=localhost;dbname=samgersh", "root", "7y_6V*87$#");
-            } catch (Exception $e) {
-                die("ERROR. Couldn't get DB Connection. " . $e->getMessage());
-            }
-            $cmd = "select player_name from sauer_matches group by player_name";
-            $stmt = $dbh->prepare($cmd);
-            $success = $stmt->execute([]);
-
-            while ($row = $stmt->fetch()) {
-                echo '<div class="player-container">' . $row["player_name"] . '</div>';
-            }
-
-            ?>
-        </div>
-
-        <div id='stats-grid'>
-            <div id='player-info'>
-                <h2>Player Info</h2>
-                <div id='player-info-grid'>
-
-                </div>
-            </div>
-            <div id='player-matches'>
-                <h2>Player's Matches</h2>
-                <div id='player-match-grid'>
-
-                </div>
+                while ($row = $stmt->fetch()) {
+                    echo '<div class="player-container" draggable="true" ondragstart="dragging_player(event)">' . $row["player_name"] . '</div>';
+                }
+                ?>
             </div>
         </div>
+
+        <div style="display: flex;">
+           <div style=" height: calc(100vh - 220px); overflow: auto; width: 20%; padding: 1rem;"> 
+                <h4>Maps</h4>
+                <img src="assets/imgs/curvy_castle.png" style="width: 100%; border-radius: 6px; margin-bottom: 1rem;"/>
+                <img src="assets/imgs/curvy_castle.png" style="width: 100%; border-radius: 6px; margin-bottom: 1rem;"/>
+                <img src="assets/imgs/curvy_castle.png" style="width: 100%; border-radius: 6px; margin-bottom: 1rem;"/>
+                <img src="assets/imgs/curvy_castle.png" style="width: 100%; border-radius: 6px; margin-bottom: 1rem;"/>
+           </div> 
+           <div id="player1" style=" border-width: 0px 2px 0px 2px; border-color: white; border-style: solid; width: 40%; padding: 1rem;" ondrop="player_dropped(event)" ondragover="allow_drop(event)"> 
+                <h4 id="player1-name"> Drag & Drop a Player</h4>
+                <div id="player1-info" class="flex flex-wrap justify-between player-info"> 
+                    <div id="player1-total-stats"> </div>
+                    <div id="player1-total-games"> </div>
+                </div>
+           </div> 
+           <div id="player2" style="width: 40%; padding: 1rem;" ondrop="player_dropped(event)" ondragover="allow_drop(event)">
+                <h4 id="player2-name"> Drag & Drop a Player</h4>
+                <div id="player2-info" class="flex flex-wrap justify-between player-info"> 
+                    <div id="player2-total-stats"> </div>
+                    <div id="player2-total-games"> </div>
+                </div>
+            </div> 
+        </div>
+
     </div>
     <script>
+
+        const state = {
+            player_dragged: undefined,
+        }
+
         window.addEventListener("load", function() {
             let matchData;
             let date;
@@ -182,89 +156,105 @@
 
             });
 
-            document.querySelectorAll(".player-container").forEach(function(el) {
-                el.addEventListener("click", function() {
-                    let name = el.innerHTML.trim();
-                    $.post({
-                        type: "POST",
-                        url: 'get_player_info.php',
-                        data: {
-                            "player": name,
-                        },
-                        success: function(response) {
-
-                            let playerMatches = JSON.parse(response);
-                            //console.log(playerMatches);
-                            let info = {
-                                "Total Games": 0,
-                                "Total Wins": 0,
-                                "Total Losses": 0,
-                                "Total Ties": 0,
-                                "Total Kills": 0,
-                                "Total Deaths": 0,
-                                "Total Suicides": 0,
-                                "Total Damage": 0,
-                                "Total Shots": 0,
-                                "Total Hits": 0,
-                                "Accuracy": 0,
-                                "K/D": 0,
-                                "Average K/D": 0,
-                                "Average Kills": 0,
-                                "Average Deaths": 0,
-                                "Average Suicides": 0,
-                                "Average Accuracy": 0,
-                            }
-                            info["Total Games"] = playerMatches.length;
-
-                            playerMatches.map(match => (info["Total Wins"] += match['win_state'] == "win" ? 1 : 0));
-                            playerMatches.map(match => (info["Total Losses"] += match['win_state'] == "lose" ? 1 : 0));
-                            playerMatches.map(match => (info["Total Ties"] += match['win_state'] == "tie" ? 1 : 0));
-
-                            playerMatches.map(match => info["Total Kills"] += parseInt(match['kills']));
-                            playerMatches.map(match => info["Total Deaths"] += parseInt(match['deaths']));
-                            playerMatches.map(match => info["Total Suicides"] += parseInt(match['suicides']));
-                            playerMatches.map(match => info["Total Shots"] += parseInt(match['total_shots']));
-                            playerMatches.map(match => info["Total Hits"] += parseInt(match['total_hits']));
-                            playerMatches.map(match => info["Total Damage"] += parseInt(match['total_damage']));
-
-                            info["Accuracy"] = parseFloat((info["Total Hits"] / info["Total Shots"] * 100).toFixed(2));
-
-                            info["K/D"] = parseFloat((info["Total Kills"] / info["Total Deaths"]).toFixed(2));
-
-                            playerMatches.map(match => info["Average K/D"] += (parseFloat((parseInt(match['kills']) / Math.max(parseInt(match['deaths']), 1)).toFixed(2))));
-                            info["Average K/D"] = parseFloat((info["Average K/D"] / playerMatches.length).toFixed(2));
-
-                            info["Average Kills"] = parseFloat((info["Total Kills"] / playerMatches.length).toFixed(2));
-                            info["Average Deaths"] = parseFloat((info["Total Deaths"] / playerMatches.length).toFixed(2));
-                            info["Average Suicides"] = parseFloat((info["Total Suicides"] / playerMatches.length).toFixed(2));
-
-                            playerMatches.map(match => info["Average Accuracy"] += parseInt(match["accuracy"]));
-                            info["Average Accuracy"] = parseFloat((info["Average Accuracy"] / playerMatches.length).toFixed(2));
-
-                            let output = "";
-                            Object.keys(info).map(function(key, index) {
-                                output += "<div>" + key + ": " + info[key] + "</div>"
-                            });
-                            document.querySelector("#player-info-grid").innerHTML = output;
-
-                            output = "";
-                            playerMatches.map(match => output += "<div class='" + match['win_state'] + "'>Match ID: " + match['id'] + " - " + match['win_state'] + "</div><br>");
-                            document.querySelector("#player-match-grid").innerHTML = output;
-
-                            //TODO: Make match view - player matches will be clickable
-                            //TODO: Add individual gun stats
-                            //TODO: Add images for maps and guns
-                            //TODO: Add cool graphs :)
-                            //TODO: sexify
-
-                            //console.log(info);
-                        }
-                    });
-                })
-            })
-
-
         })
+
+        function allow_drop(event) { event.preventDefault(); }
+
+        function player_dropped( event ) {
+            event.preventDefault(); 
+            console.log("Dropped!", event.originalTarget);
+
+
+            let players = ["player1", "player2"];
+            let element = event.originalTarget;
+            while ( players.includes( element.id ) === false ) {
+                element = element.parentElement;
+            }
+            const player_container = element.id;   // Will be either "player1" or "player2"
+
+            // Update the Player Container name to the Player thats been dropped in
+            document.getElementById(`${player_container}-name`).innerText = state.player_dragged;
+
+            $.post({
+                type: "POST",
+                url: 'get_player_info.php',
+                data: {
+                    "player": state.player_dragged,
+                },
+                success: function(response) {
+
+                    let playerMatches = JSON.parse(response);
+                    //console.log(playerMatches);
+                    let info = {
+                        "Total Games": 0,
+                        "Total Wins": 0,
+                        "Total Losses": 0,
+                        "Total Ties": 0,
+                        "Total Kills": 0,
+                        "Total Deaths": 0,
+                        "Total Suicides": 0,
+                        "Total Damage": 0,
+                        "Total Shots": 0,
+                        "Total Hits": 0,
+                        "Accuracy": 0,
+                        "K/D": 0,
+                        "Average K/D": 0,
+                        "Average Kills": 0,
+                        "Average Deaths": 0,
+                        "Average Suicides": 0,
+                        "Average Accuracy": 0,
+                    }
+                    info["Total Games"] = playerMatches.length;
+
+                    playerMatches.map(match => (info["Total Wins"] += match['win_state'] == "win" ? 1 : 0));
+                    playerMatches.map(match => (info["Total Losses"] += match['win_state'] == "lose" ? 1 : 0));
+                    playerMatches.map(match => (info["Total Ties"] += match['win_state'] == "tie" ? 1 : 0));
+
+                    playerMatches.map(match => info["Total Kills"] += parseInt(match['kills']));
+                    playerMatches.map(match => info["Total Deaths"] += parseInt(match['deaths']));
+                    playerMatches.map(match => info["Total Suicides"] += parseInt(match['suicides']));
+                    playerMatches.map(match => info["Total Shots"] += parseInt(match['total_shots']));
+                    playerMatches.map(match => info["Total Hits"] += parseInt(match['total_hits']));
+                    playerMatches.map(match => info["Total Damage"] += parseInt(match['total_damage']));
+
+                    info["Accuracy"] = parseFloat((info["Total Hits"] / info["Total Shots"] * 100).toFixed(2));
+
+                    info["K/D"] = parseFloat((info["Total Kills"] / info["Total Deaths"]).toFixed(2));
+
+                    playerMatches.map(match => info["Average K/D"] += (parseFloat((parseInt(match['kills']) / Math.max(parseInt(match['deaths']), 1)).toFixed(2))));
+                    info["Average K/D"] = parseFloat((info["Average K/D"] / playerMatches.length).toFixed(2));
+
+                    info["Average Kills"] = parseFloat((info["Total Kills"] / playerMatches.length).toFixed(2));
+                    info["Average Deaths"] = parseFloat((info["Total Deaths"] / playerMatches.length).toFixed(2));
+                    info["Average Suicides"] = parseFloat((info["Total Suicides"] / playerMatches.length).toFixed(2));
+
+                    playerMatches.map(match => info["Average Accuracy"] += parseInt(match["accuracy"]));
+                    info["Average Accuracy"] = parseFloat((info["Average Accuracy"] / playerMatches.length).toFixed(2));
+
+                    let output = "";
+                    Object.keys(info).map(function(key, index) {
+                        output += "<div>" + key + ": " + info[key] + "</div>"
+                    });
+                    document.querySelector(`#${player_container}-total-stats`).innerHTML = output;
+
+                    output = "";
+                    playerMatches.map(match => output += "<div class='" + match['win_state'] + "'>Match ID: " + match['id'] + " - " + match['win_state'] + "</div><br>");
+                    document.querySelector(`#${player_container}-total-games`).innerHTML = output;
+
+                    //TODO: Make match view - player matches will be clickable
+                    //TODO: Add individual gun stats
+                    //TODO: Add images for maps and guns
+                    //TODO: Add cool graphs :)
+                    //TODO: sexify
+
+                    //console.log(info);
+                }
+            })
+        }
+
+        function dragging_player(event) {
+            state.player_dragged = event.originalTarget.innerHTML.trim();
+        }
     </script>
 </body>
 
